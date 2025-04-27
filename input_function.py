5e8916b6a14d9d046eb2783bdd6d6b2f843f3bbe
Kevin Harding, MIS <146769989+KHard2Bme@users.noreply.github.com>
2025-04-26 23:48:45 -0400

    new files ready

---
+++ input_function.py
@@ -0,0 +1,4 @@
+hello = "Hello"
+name = input("Whats's your name?\n")
+greeting = hello +" "+name
+print(greeting)
--- tax.py
+++ tax.py
@@ -2,3 +2,7 @@ amount = 200
 tax = 0.07
 total = amount + amount*tax
 print(total)
+hello = "Hello"
+name = "Sarah"
+greeting = hello +" "+ name
+print(greeting)
